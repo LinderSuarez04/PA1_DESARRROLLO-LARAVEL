3b5e57c226a0378d0ac926a6969dfeeba124d867
RomelLinares <lrubiormel@uss.edu.pe>
2024-10-27 18:18:09 -0500

    Plantilla

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
 Route::get('/', function () {
     return view('login');
+});
+
+Route::get('/inicio', function () {
+    return view('plantilla/app');
 });
